b919479165aa228093c50d3d22d336c7b1d88de1
Vassilis <vchoutas@VassilisLaptop.(none)>
2014-11-16 02:41:48 +0200

    Delete OpenMP macros from memcpy

--- Work1/OpenMP/data_rearrangement.c
+++ Work1/OpenMP/data_rearrangement.c
@@ -10,11 +10,6 @@ void data_rearrangement(float *Y, float *X,
 			int N){
 
   int i=0;
-  omp_set_num_threads(THREADS);
-  int threads = omp_get_num_threads();
-  //~ int chunk = N  /  ( 4*DIM* threads ) ;
-  //~ #pragma omp parallel for schedule(dynamic , chunk) shared(X,Y) private(i)
-  #pragma omp parallel for schedule(guided) shared(X,Y) private(i)
   for(i=0; i<N; i++){
     memcpy(&Y[i*DIM], &X[permutation_vector[i]*DIM], DIM*sizeof(float));
   }
