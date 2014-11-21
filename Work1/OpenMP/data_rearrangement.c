f40f5f44ddfd1fe7a37b83f87961209f32e9860b
VasilisH <vasilis4ch@gmail.com>
2014-11-21 16:26:16 +0200

    Parallelized data rearrangement

--- Work1/OpenMP/data_rearrangement.c
+++ Work1/OpenMP/data_rearrangement.c
@@ -10,8 +10,24 @@ void data_rearrangement(float *Y, float *X,
 			int N){
 
   int i=0;
-  for(i=0; i<N; i++){
-    memcpy(&Y[i*DIM], &X[permutation_vector[i]*DIM], DIM*sizeof(float));
+  #pragma omp parallel  shared(X,Y,permutation_vector)
+  #pragma private(index,i)
+  {
+    #pragma omp for schedule(guided) nowait
+    for(i=0; i<N; i++){
+      memcpy(&Y[i*DIM], &X[permutation_vector[i]*DIM], DIM*sizeof(float));
+    }
   }
-
+  //~ int index;
+  //~ #pragma omp parallel  shared(X,Y,permutation_vector)
+  //~ #pragma private(index,i)
+  //~ {
+    //~ #pragma omp for schedule(guided) nowait
+    //~ for(i=0; i<N; i++){
+      //~ index = permutation_vector[i]*DIM ;
+      //~ Y[i*DIM] = X[ index ];
+      //~ Y[i*DIM + 1] = X[ index + 1 ];
+      //~ Y[i*DIM + 2] = X[ index + 2 ];
+    //~ }
+  //~ }
 }
