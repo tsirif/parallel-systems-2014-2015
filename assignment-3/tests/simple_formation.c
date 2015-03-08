4fef8da8ddf017a52c034b7dfabf6984fc1b033e
orestisf <orestisf1993@gmail.com>
2015-03-08 19:17:57 +0200

    Adds more test formations

--- assignment-3/tests/simple_formation.c
+++ assignment-3/tests/simple_formation.c
@@ -21,30 +21,46 @@ void save_table(const int *X, const char* filename, int N)
 #define N 6
 #define TOT 36
 const int block_formation[TOT] = {
-    0, 0, 0, 0, 0,0,
-    0, 0, 1, 1, 0,0,
-    0, 0, 1, 1, 0,0,
-    0, 0, 0, 0, 0,0,
-    0, 0, 0, 0, 0,0
+    0, 0, 0, 0, 0, 0,
+    0, 0, 1, 1, 0, 0,
+    0, 0, 1, 1, 0, 0,
+    0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0
 };
 
 const int blinker_formation[TOT] = {
-    0, 0, 0, 0, 0,0,
-    0, 0, 1, 0, 0,0,
-    0, 0, 1, 0, 0,0,
-    0, 0, 1, 0, 0,0,
-    0, 0, 0, 0, 0,0
+    0, 0, 0, 0, 0, 0,
+    0, 0, 1, 0, 0, 0,
+    0, 0, 1, 0, 0, 0,
+    0, 0, 1, 0, 0, 0,
+    0, 0, 0, 0, 0, 0
 };
 
+const int blinker_and_block_formation[4 * TOT] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0,
+    0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+};
+
+const int four_in_a_row[TOT] = {
+    0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 0,
+    0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0
+};
 
 int main()
 {
-    //~ for (int i=0; i<TOT; i++) printf("%d ", block_formation[i]);
-    //~ printf("\n");
-    //~ int *a = malloc(TOT*sizeof(int));
     save_table(block_formation, "block.bin", N);
     save_table(blinker_formation, "blinker.bin", N);
-    //~ read_from_file(a, "block.bin", N);    
-    //~ for (int i=0; i<TOT; i++) printf("%d ", a[i]);
-    //~ printf("\n");
+    save_table(four_in_a_row, "4row.bin", N);
+    save_table(blinker_and_block_formation, "blinker_block.bin", 2*N);
 }
