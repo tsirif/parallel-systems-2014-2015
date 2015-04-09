1382b7d335a66287424b0cf4688298fdbabcc70c
orestisf <orestisf1993@gmail.com>
2015-04-09 04:00:11 +0300

    Adds generic test function which uses a generator function for input.

--- assignment-3/tests/test_conv.cu
+++ assignment-3/tests/test_conv.cu
@@ -1,8 +1,12 @@
 #define TESTING
 #include "../cuda/cuda-int.c"
+#include <time.h>
 
 #define mu_assert(message, test) do { if (!(test)) return message; } while (0)
 #define mu_run_test(test) do { const char *message = test(); tests_run++; \
+        if (message) return message; } while (0)
+
+#define mu_run_test_wargs(test, ...) do { const char *message = test(__VA_ARGS__); tests_run++; \
         if (message) return message; } while (0)
 
 int tests_run;
@@ -33,12 +37,21 @@ void print_table(int *A, int N)
 
 uint mem_size;
 uint mem_size_tiled;
+
+/* Host pointers */
 int *table;
 uint *table_tiled;
-uint *d_table_tiled;
-int *d_table;
 int *table_final;
 
+/* Cuda device pointers */
+uint *d_table_tiled;
+int *d_table;
+
+/* grid and block dimensions */
+dim3 grid;
+dim3 block;
+
+/* Initializes global variables.*/
 static void init()
 {
     table = NULL;
@@ -46,24 +59,23 @@ static void init()
     d_table_tiled = NULL;
     d_table = NULL;
     table_final = NULL;
+    srand(time(NULL));
 }
 
-
-static const char *test_zeros_to()
+/* Allocates size for an array of 32 elements. */
+static void init_32()
 {
-
     mem_size = 32 * sizeof(int);
     mem_size_tiled = sizeof(uint);
 
-    const dim3 grid(1, 1);
-    const dim3 block(1, 1);
+    grid = dim3(1, 1);
+    block = dim3(1, 1);
 
     table = (int *) realloc(table, mem_size);
     table_tiled = (uint *) realloc(table_tiled, mem_size_tiled);
     table_final = (int *) realloc(table_final, mem_size);
 
-    for (int i = 0; i < 32; i++) table[i] = 0;
-
+    /* Free cuda pointers just in case. */
     cudaFree(d_table);
     cudaFree(d_table_tiled);
 
@@ -72,6 +84,30 @@ static const char *test_zeros_to()
 
     cudaMalloc((void **) &d_table_tiled, mem_size_tiled);
     cudaCheckErrors("device allocation of GOL uint tiled matrix failed", __FILE__, __LINE__);
+}
+
+inline int zero_gen(){
+    return 0;
+}
+
+inline int one_gen(){
+    return 1;
+}
+
+inline int rand_gen(){
+    return ( (float)rand() / (float)RAND_MAX ) < 0.4;
+}
+
+/* Run test using a generator function. */
+static const char *test_wgen(int (*generator)())
+{
+
+    uint expected_result = 0;
+
+    for (int i = 0; i < 32; i++) {
+        table[i] = generator();
+        expected_result += table[i] << i;
+    }
 
     cudaMemcpy(d_table, table, mem_size, cudaMemcpyHostToDevice);
     cudaCheckErrors("copy from host to device memory failed", __FILE__, __LINE__);
@@ -82,7 +118,7 @@ static const char *test_zeros_to()
     cudaMemcpy(table_tiled, d_table_tiled, mem_size_tiled, cudaMemcpyDeviceToHost);
     cudaCheckErrors("memcpy to tiled", __FILE__, __LINE__);
 
-    mu_assert("Unexpected tiled value!", table_tiled[0] == 0);
+    mu_assert("Unexpected tiled value!", table_tiled[0] == expected_result);
 
     convert_from_tiled <<< grid, block >>>(d_table, d_table_tiled, 1, 1, 1);
     cudaCheckErrors("failed to convert to normal repr from uint tiled repr", __FILE__, __LINE__);
@@ -91,7 +127,6 @@ static const char *test_zeros_to()
     cudaCheckErrors("memcpy to final", __FILE__, __LINE__);
 
     int result = 1;
-
     for (int i = 0; i < 32; i++) result &= (table_final[i] == table[i]);
 
     mu_assert("Initial and final array differ!", result);
@@ -99,14 +134,33 @@ static const char *test_zeros_to()
     return 0;
 }
 
-static const char *all_tests(){
-    mu_run_test(test_zeros_to);
+/* Run all tests. Also times the execution. */
+static const char *all_tests()
+{
+    init_32();
+
+    float time;
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start, 0);
+
+    mu_run_test_wargs(test_wgen, one_gen);
+    mu_run_test_wargs(test_wgen, zero_gen);
+    mu_run_test_wargs(test_wgen, rand_gen);
+    mu_run_test_wargs(test_wgen, rand_gen);
+    mu_run_test_wargs(test_wgen, rand_gen);
+
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&time, start, stop);
+    printf("CUDA tests time to run:  %f s \n", time / 1000);
+
     return 0;
 }
 
 int main()
 {
-
     init();
     const char *result = all_tests();
 
@@ -118,12 +172,4 @@ int main()
     printf("Tests run: %d\n", tests_run);
 
     return result != 0;
-
-
-    // ~ srand(time(NULL));
-    // ~ for (int i = 0; i < 32; i++) table[i] = ( (float)rand() / (float)RAND_MAX ) < 0.4;
-    // ~ printf("%u\n%x\n", table_tiled[0], table_tiled[0]);
-    // ~ for (int i = 0; i < 32; i++) printf("%d ", table_final[i]);
-    // ~ printf("\n");
-
 }
